ede245177690117f90e0b07732ea7ca2cd3da7bb
William Gannon <wacko1969@protonmail.com>
2026-01-19 19:40:10 -0500

    feat: update webpack configuration to improve production build process and add missing start:prod script

--- webpack.config.js
+++ webpack.config.js
@@ -9,110 +9,115 @@ const smp = new SpeedMeasurePlugin({ disable: false });
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// eslint-disable-next-line no-undef
-const isDev = process.env.NODE_ENV !== 'production';
+export default (env, argv) => {
+  const isDev = argv && argv.mode !== 'production';
 
-const config = {
-  entry: './src/index.tsx',
-  devtool: false,
-  stats: {
-    errorDetails: true,
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].chunk.js',
-    clean: true,
-    publicPath: '/',
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@ui': path.resolve(__dirname, 'src/ui'),
-      '@lib': path.resolve(__dirname, 'src/lib'),
-      '@styles': path.resolve(__dirname, 'src/styles'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
+  const config = {
+    entry: './src/index.tsx',
+    devtool: false,
+    stats: {
+      errorDetails: true,
     },
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendor',
-          chunks: 'all',
-        },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].chunk.js',
+      clean: true,
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@assets': path.resolve(__dirname, 'src/assets'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@ui': path.resolve(__dirname, 'src/ui'),
+        '@lib': path.resolve(__dirname, 'src/lib'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
       },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'esbuild-loader',
-        options: {
-          target: 'es2020',
-          loader: 'tsx',
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+          },
         },
       },
-      {
-        test: /\.(png|jpe?g|gif|webp|avif|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[name].[contenthash][ext]',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          loader: 'esbuild-loader',
+          options: {
+            target: 'es2020',
+            loader: 'tsx',
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
-      },
+        {
+          test: /\.(png|jpe?g|gif|webp|avif|svg)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name].[contenthash][ext]',
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        inject: 'body',
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].chunk.css',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      inject: 'body',
-    }),
-    new webpack.SourceMapDevToolPlugin({
-      filename: '[file].map',
-      exclude: /vendor/,
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      port: 3000,
+      hot: true,
+      open: true,
+      historyApiFallback: true,
     },
-    port: 3000,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
-  },
-  performance: {
-    hints: 'warning',
-    maxEntrypointSize: Infinity,
-    maxAssetSize: Infinity,
-  },
-  cache: {
-    type: 'filesystem',
-    buildDependencies: {
-      config: [__filename],
+    performance: {
+      hints: 'warning',
+      maxEntrypointSize: Infinity,
+      maxAssetSize: Infinity,
     },
-  },
+    cache: {
+      type: 'filesystem',
+      buildDependencies: {
+        config: [__filename],
+      },
+    },
+  };
+
+  // Only enable source maps in development
+  if (isDev) {
+    config.plugins.push(
+      new webpack.SourceMapDevToolPlugin({
+        filename: '[file].map',
+        exclude: /vendor/,
+      })
+    );
+  }
+
+  const smpconfig = smp.wrap(config, ['MiniCssExtractPlugin']);
+  return smpconfig;
 };
-
-if (!isDev) {
-  config.plugins.push(
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',
-      chunkFilename: '[name].[contenthash].chunk.css',
-    })
-  );
-}
-
-const smpconfig = smp.wrap(config);
-
-export default smpconfig;
